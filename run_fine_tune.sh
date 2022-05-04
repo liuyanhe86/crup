958af4d1b601662675ff8ac2ec05afa48c7af3fb
Liu Yanhe <liuyanhe@seu.edu.com>
2022-05-04 19:44:17 +0800

    22.5.4 Update data.

--- run_fine_tune.sh
+++ run_fine_tune.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
 
 # python main.py --dataset coarse-few-nerd --protocol fine-tune --model ProtoNet --dot --use_sgd
-# python main.py --dataset fine-few-nerd --protocol fine-tune --model ProtoNet --dot --use_sgd
-python main.py --dataset stackoverflow --protocol fine-tune --model ProtoNet --dot --use_sgd
+python main.py --dataset fine-few-nerd --protocol fine-tune --model ProtoNet --dot --use_sgd
+# python main.py --dataset stackoverflow --protocol fine-tune --model ProtoNet --dot --use_sgd
