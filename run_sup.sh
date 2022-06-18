326bedee8aede7479f294d3b5a8a8100a58b4300
LiuYH <liuyanhe@seu.edu.cn>
2022-06-18 21:10:45 +0800

    22.6.18

--- run_sup.sh
+++ run_sup.sh
@@ -1,23 +1,24 @@
-export CUDA_VISIBLE_DEVICES=2
-
-python main.py \
-        --dataset few-nerd \
-        --setting sup \
-        --model Bert-Tagger \
-        --lr 5e-3 \
-        --use_sgd \
-        --train_encoder
+export CUDA_VISIBLE_DEVICES=1
 
 # python main.py \
 #         --dataset few-nerd \
 #         --setting sup \
-#         --model ProtoNet \
-#         --lr 2e-5 \
-#         --dot
+#         --model Bert-Tagger \
+#         --lr 5e-3 \
+#         --use_sgd \
+#         --train_encoder
+
+python main.py \
+        --dataset few-nerd \
+        --setting sup \
+        --model ProtoNet \
+        --lr 2e-5 \
+        --dot
 
 # python main.py \
 #         --dataset few-nerd \
 #         --setting sup \
 #         --model PCP \
 #         --augment \
-        # --train_encoder
+#         --lr 0.1 \
+#         --only_train_encoder
