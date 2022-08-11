2ba5d2787c5970a214b7bc4700e9a61e77e0a11b
柳沿河 <62291319+liuyanhe86@users.noreply.github.com>
2022-08-11 21:06:22 +0800

    Delete run_multitask.sh

--- run_multitask.sh
+++
@@ -1,5 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-
-# python main.py --dataset coarse-few-nerd --setting multi-task --model ProtoNet --dot
-python main.py --dataset fine-few-nerd --setting multi-task --model ProtoNet --dot
-# python main.py --dataset stackoverflow --setting multi-task --model ProtoNet --dot
